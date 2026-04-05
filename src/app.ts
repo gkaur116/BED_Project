20a83f571d4e880a5f357f0987a743d4a231c2f5
Gursimran Kaur <gkaur116@academic.rrc.ca>
2026-04-04 22:40:03 -0500

    Register all routes in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Express } from "express";
 import morgan from "morgan";
 import healthRoutes from "./api/v1/routes/healthRoutes";
+import menuItemRoutes from "./api/v1/routes/menuItemRoutes";
+import orderRoutes from "./api/v1/routes/orderRoutes";
+import reviewRoutes from "./api/v1/routes/reviewRoutes";
 
 // Initialize Express application
 const app: Express = express();
@@ -11,5 +14,8 @@ app.use(express.json());
 
 // Routes
 app.use("/api/v1", healthRoutes);
+app.use("/api/v1", menuItemRoutes);
+app.use("/api/v1", orderRoutes);
+app.use("/api/v1", reviewRoutes);
 
 export default app;
