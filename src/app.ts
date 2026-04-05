2f8fd01e739de98f9316477267dc6e6d0bb6aefe
Gursimran Kaur <gkaur116@academic.rrc.ca>
2026-04-04 20:34:21 -0500

    Updated app.ts with morgan

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express, { Express } from "express";
+import morgan from "morgan";
 import healthRoutes from "./api/v1/routes/healthRoutes";
 
 // Initialize Express application
 const app: Express = express();
 
-// Define a route
+// Middleware
+app.use(morgan("combined"));
+app.use(express.json());
+
+// Routes
 app.use("/api/v1", healthRoutes);
 
 export default app;
