1dd2e5d9673f0692abf4cfee5b0823bf845a7276
Gursimran Kaur <gkaur116@academic.rrc.ca>
2026-04-11 19:45:08 -0500

    Added auth and role protection to all routes

--- src/api/v1/routes/reviewRoutes.ts
+++ src/api/v1/routes/reviewRoutes.ts
@@ -1,12 +1,35 @@
 import express, { Router } from "express";
-import { getReviewsByMenuItemId, createReview, deleteReview } from "../controllers/reviewController";
+import {
+  getReviewsByMenuItemId,
+  createReview,
+  deleteReview,
+} from "../controllers/reviewController";
 import { validateRequest } from "../middleware/validate";
 import { reviewSchemas } from "../validation/reviewSchemas";
+import authenticate from "../middleware/authenticate";
+import isAuthorized from "../middleware/authorize";
 
 const router: Router = express.Router();
 
-router.get("/reviews/:menuItemId", validateRequest(reviewSchemas.getByMenuItemId), getReviewsByMenuItemId);
-router.post("/reviews", validateRequest(reviewSchemas.create), createReview);
-router.delete("/reviews/:id", validateRequest(reviewSchemas.delete), deleteReview);
+router.get(
+  "/reviews/:menuItemId",
+  authenticate,
+  validateRequest(reviewSchemas.getByMenuItemId),
+  getReviewsByMenuItemId
+);
+router.post(
+  "/reviews",
+  authenticate,
+  isAuthorized({ hasRole: ["admin", "user"] }),
+  validateRequest(reviewSchemas.create),
+  createReview
+);
+router.delete(
+  "/reviews/:id",
+  authenticate,
+  isAuthorized({ hasRole: ["admin"] }),
+  validateRequest(reviewSchemas.delete),
+  deleteReview
+);
 
 export default router;
