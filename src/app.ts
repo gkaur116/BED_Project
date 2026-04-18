db1be8763288a3d24ae985a57d8b03b02486edfa
Gursimran Kaur <gkaur116@academic.rrc.ca>
2026-04-18 00:33:41 -0500

    Updated app.ts with swagger helmet cors and dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
+import dotenv from "dotenv";
+// Load environment variables BEFORE your internal imports!
+dotenv.config();
+
 import express, { Express } from "express";
+import { getHelmetConfig } from "./config/helmetConfig";
+import cors from "cors";
+import { getCorsOptions } from "./config/corsConfig";
 import {
   accessLogger,
   errorLogger,
   consoleLogger,
 } from "./api/v1/middleware/logger";
 import errorHandler from "./api/v1/middleware/errorHandler";
+import setupSwagger from "./config/swagger";
 import healthRoutes from "./api/v1/routes/healthRoutes";
 import menuItemRoutes from "./api/v1/routes/menuItemRoutes";
 import orderRoutes from "./api/v1/routes/orderRoutes";
@@ -14,6 +22,10 @@ import adminRoutes from "./api/v1/routes/adminRoutes";
 
 // Initialize Express application
 const app: Express = express();
+
+// Security middleware
+app.use(getHelmetConfig());
+app.use(cors(getCorsOptions()));
 
 // Logging middleware
 if (process.env.NODE_ENV === "production") {
@@ -25,6 +37,9 @@ if (process.env.NODE_ENV === "production") {
 
 // Body parsing middleware
 app.use(express.json());
+
+// Swagger documentation
+setupSwagger(app);
 
 // Routes
 app.use("/api/v1", healthRoutes);
